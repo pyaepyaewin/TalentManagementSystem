e7591f174dedd62c1d1d6e2d8af0c4f1aff30ce5
Pyae_Pyae <you@example.com>
2019-09-03 22:58:52 +0630

    UI Update

--- app/src/main/java/com/example/talentmanagementsystem/util/AppConstants.kt
+++ app/src/main/java/com/example/talentmanagementsystem/util/AppConstants.kt
@@ -3,6 +3,6 @@ package com.example.talentmanagementsystem.util
 object AppConstants {
   //var base_url="http://192.168.99.20/TMA/public/api/v1/"
   // var base_url="http://localhost/TMA/public/api/v1/"
-  var base_url="http://10.10.21.62/TMA/public/api/v1/"
+  var base_url="http://10.0.2.2/TMA/public/api/v1/"
 
 }
